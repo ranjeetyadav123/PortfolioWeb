69d6ad906f9da29e44cda3dfb872003f9c3d192d
Ranjeet Yadav <ranjeetsyadav40@gmail.com>
2022-03-08 15:43:02 +0530

    Updates

---
+++ precache-manifest.015aa5aeb3d5f5a474c12d6986f65b8f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2c18ba1778cd0ffa58c",
+    "url": "/PortfolioWeb/static/css/main.87db814a.chunk.css"
+  },
+  {
+    "revision": "a2c18ba1778cd0ffa58c",
+    "url": "/PortfolioWeb/static/js/main.8c7fc14c.chunk.js"
+  },
+  {
+    "revision": "cee8ca834776aa451724",
+    "url": "/PortfolioWeb/static/js/runtime~main.64587ce1.js"
+  },
+  {
+    "revision": "1804cf13d021b2a59e3e",
+    "url": "/PortfolioWeb/static/css/2.47240a12.chunk.css"
+  },
+  {
+    "revision": "1804cf13d021b2a59e3e",
+    "url": "/PortfolioWeb/static/js/2.f7746382.chunk.js"
+  },
+  {
+    "revision": "03536cb0a75ce125b824",
+    "url": "/PortfolioWeb/static/js/3.d0e5de46.chunk.js"
+  },
+  {
+    "revision": "52423f17570ea8bc97453df553b354b6",
+    "url": "/PortfolioWeb/static/media/phone.52423f17.png"
+  },
+  {
+    "revision": "775dfe7f630b37076c29d0e16e21ffcf",
+    "url": "/PortfolioWeb/static/media/email.775dfe7f.png"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/PortfolioWeb/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/PortfolioWeb/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/PortfolioWeb/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/PortfolioWeb/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/PortfolioWeb/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/PortfolioWeb/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/PortfolioWeb/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PortfolioWeb/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PortfolioWeb/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PortfolioWeb/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PortfolioWeb/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PortfolioWeb/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "48ac33720089b904058d7692729f0ed3",
+    "url": "/PortfolioWeb/static/media/RanjeetPic.48ac3372.jpg"
+  },
+  {
+    "revision": "cabad0e436f8e1a53f278c6c61206033",
+    "url": "/PortfolioWeb/static/media/shape-bg1.cabad0e4.png"
+  },
+  {
+    "revision": "ff9073ff050ed3e8247c060835585423",
+    "url": "/PortfolioWeb/static/media/bg3.ff9073ff.png"
+  },
+  {
+    "revision": "27b3731b7127c940b36c2f90352b0cf9",
+    "url": "/PortfolioWeb/index.html"
+  }
+];
